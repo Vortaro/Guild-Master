38f31495762cf6177d2879afac981deeb8771c7e
Christopher <shizen37@gmail.com>
2011-09-08 20:37:17 -0400

    fixed typo in goods.db and work on trade interface is underway

--- economy.lisp
+++ economy.lisp
@@ -19,19 +19,19 @@
 (defvar *quantity* 0) ;; How much the merchant wants to buy or sell
 (defparameter *money* 10000) ;; How much money the merchant has
 
-(defun print-money ()
+(defun print-money () ;; Shows how much money the player has
 	(format t "Current funds: ~d~%" *money*))
 
 (let ((price 0) (new-supply 0) (supply 20) (rate 14.2) (initial-price 2050))
-(defun print-price ()
-	(format t "Price: ~d~%" price))
-(defun linear ()
+(defun print-price () ;; Shows how much the unit cost; will be replaced by a
+	(format t "Price: ~d~%" price)) ;; price averager
+(defun linear () ;; A linear plot for any good
 	(setf price (round (+ initial-price (* supply (- rate))))))
 (defun subtract-money ()
 	(setf *money* (- *money* price)))
 (defun add-money ()
 	(setf *money* (+ *money* price)))
-(defun find-new-buy-supply ()
+(defun find-new-buy-supply () ;; Necessary to get the loop to stop
 	(setf new-supply (- supply *quantity*)))
 (defun find-new-sell-supply ()
 	(setf new-supply (+ supply *quantity*)))
@@ -63,3 +63,36 @@
 		(add-money)
 		(increase-supply))
 	(print-money)))
+
+;; Menus
+
+(defparameter *trade-menu*
+	`(("Buy" . ,(lambda () (format t "What would you like to buy?~%")))
+		("Sell" . ,(lambda () (format t "What would you like to sell?~%")))
+		("Leave" . ,(lambda () (format t "Come again sometime.~%")))))
+(defvar *goods-db* nil)
+
+(defun load-goods-db (&optional (goods.db "./goods.db"))
+  (with-open-file (in goods.db) (read in)))
+
+
+(defun trade-interface-start ()
+	(market-menu "Welcome to the market." *trade-menu*))
+
+(defun market-menu (title entries)
+	(format t "~a~%" title)
+	(dolist (entry entries)
+    (format t "~a~%" (car entry)))
+	(format t "Type the word corresponding to your choice.~%Typing leave will return you to the city.~%")
+	(let ((input (read-line)))
+		(dolist (entry entries)
+			(when (string-equal input (car entry))
+        (funcall (cdr entry)) (return)))
+		(unless
+				(dolist (entry entries)
+					(when (string-equal input (car entry))
+						(funcall (cdr entry)) (return t)))
+			(format t "~%~%That's not an option.~%")
+			(trade-interface-start))))
+
+(trade-interface-start)
